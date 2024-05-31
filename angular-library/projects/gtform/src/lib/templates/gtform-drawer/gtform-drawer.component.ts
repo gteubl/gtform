a30f664e3b630f963a873621fa34cf1918ba33ba
Guilherme Teubl <gteubl@gmail.com>
2024-05-31 09:23:46 +0200

    drawer

--- angular-library/projects/gtform/src/lib/templates/gtform-drawer/gtform-drawer.component.ts
+++ angular-library/projects/gtform/src/lib/templates/gtform-drawer/gtform-drawer.component.ts
@@ -1,13 +1,14 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, filter } from 'rxjs';
 
 import { GtformDrawerService } from '../../services/index';
 
 @Component({
   selector: 'gtform-drawer',
   templateUrl: './gtform-drawer.component.html',
-  styleUrl: './gtform-drawer.component.scss'
+  styleUrl: './gtform-drawer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GtformDrawerComponent implements OnInit, AfterViewInit {
   @Input() public alwaysShowDrawer = false;
@@ -20,26 +21,35 @@ export class GtformDrawerComponent implements OnInit, AfterViewInit {
   private hasRightDrawerContent = new BehaviorSubject(false);
   public hasRightDrawerContent$ = this.hasRightDrawerContent.asObservable();
 
-
   private hasLeftDrawerContent = new BehaviorSubject(false);
   public hasLeftDrawerContent$ = this.hasLeftDrawerContent.asObservable();
 
-  public constructor(private drawerService: GtformDrawerService) {
+  public constructor(private drawerService: GtformDrawerService, private cdr: ChangeDetectorRef) {
   }
 
   public ngOnInit(): void {
-    this.drawerService.leftDrawerState$.subscribe(state => {
-      this.isLeftDrawerOpen = state;
-    });
+    this.drawerService.leftDrawerState$
+      .pipe(
+        filter(() => this.hasLeftDrawerContent.value)
+      )
+      .subscribe(state => {
+        this.isLeftDrawerOpen = state;
+        this.cdr.detectChanges();
+      });
 
-    this.drawerService.rightDrawerState$.subscribe(state => {
-      this.isRightDrawerOpen = state;
-    });
+    this.drawerService.rightDrawerState$
+      .pipe(
+        filter(() => this.hasRightDrawerContent.value)
+      ).subscribe(state => {
+        this.isRightDrawerOpen = state;
+        this.cdr.detectChanges();
+      });
   }
 
   public ngAfterViewInit(): void {
     this.hasRightDrawerContent.next(this.rightDrawerElement?.nativeElement.children.length > 0);
-    this.hasLeftDrawerContent.next( this.leftDrawerElement?.nativeElement.children.length > 0);
+    this.hasLeftDrawerContent.next(this.leftDrawerElement?.nativeElement.children.length > 0);
+    this.cdr.detectChanges();
   }
 
 }
