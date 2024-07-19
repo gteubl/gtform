8f47ff1c6f53dca6efc1a27713dc2a5c764ddb66
Guilherme Teubl <gteubl@gmail.com>
2024-07-19 09:51:18 +0200

    bug

--- angular-library/projects/gtform/src/lib/templates/gtform-drawer/gtform-drawer.module.ts
+++ angular-library/projects/gtform/src/lib/templates/gtform-drawer/gtform-drawer.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { GtformDrawerComponent } from './gtform-drawer.component';
 
 @NgModule(
   {
     imports: [
-      BrowserModule,
       CommonModule
     ],
     declarations: [GtformDrawerComponent],
