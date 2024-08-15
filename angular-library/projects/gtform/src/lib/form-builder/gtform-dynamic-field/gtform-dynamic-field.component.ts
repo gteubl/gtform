745ccaa2108ccd03b66e7af740c108dd2f4e2afd
Guilherme Teubl <gteubl@gmail.com>
2024-08-15 14:36:13 +0200

    ngchanges

--- angular-library/projects/gtform/src/lib/form-builder/gtform-dynamic-field/gtform-dynamic-field.component.ts
+++ angular-library/projects/gtform/src/lib/form-builder/gtform-dynamic-field/gtform-dynamic-field.component.ts
@@ -1,96 +1,102 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Component, Input, OnDestroy, OnInit, Type, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, Type, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
-import { Observable, takeWhile } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
-import { ComponentType } from '../../models/index';
+import { Observable } from 'rxjs';
+
+
 import { GtformDynamicFieldDirective } from '../directives/gtform-dynamic-field.directive';
 import { ControlConfig } from '../models/control-config';
 import { GtformDynamicFieldService } from '../services/gtform-dynamic-field.service';
 
 @Component({
   selector: 'gtform-dynamic-field',
-  templateUrl: './gtform-dynamic-field.component.html',
-  styleUrl: './gtform-dynamic-field.component.scss'
+  templateUrl: './gtform-dynamic-field.component.html'
 })
-export class GtformDynamicFieldComponent implements OnInit, OnDestroy {
-
+export class GtformDynamicFieldComponent implements OnInit, OnDestroy, OnChanges {
   @Input() public config!: ControlConfig;
   @ViewChild(GtformDynamicFieldDirective, { static: true }) public dynamicField!: GtformDynamicFieldDirective;
   private isAlive: boolean = true;
+  private componentRef: any;
   //FormGroups
   @Input() public formGroup: FormGroup | undefined;
 
   public constructor(private fieldService: GtformDynamicFieldService) {
   }
 
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['config']) {
+      this.updateComponent();
+    }
+  }
+
   public ngOnInit(): void {
-    const componentType = this.fieldService.getComponentForField(this.config.componentType) as Type<any>;
-    const componentRef = this.dynamicField.viewContainerRef.createComponent(componentType);
+    this.createComponent();
+  }
 
-    // Standardize and set up inputs dynamically
-    const standardizedConfig = this.standardizeConfig(this.config);
+  public ngOnDestroy(): void {
+    this.isAlive = false;
+  }
 
-    // Apply the standardized configuration to the component instance, checking for observables
-    Object.keys(standardizedConfig).forEach(inputName => {
-      const inputValue = standardizedConfig[inputName];
-
-      if (inputValue instanceof Observable) {
-        // Subscribe to the observable and assign the emitted value
-        inputValue.pipe(takeWhile(() => this.isAlive)).subscribe(value => {
-          (componentRef.instance as any)[inputName] = value;
-        });
-      } else {
-        // Directly assign the value if it's not an observable
-        (componentRef.instance as any)[inputName] = inputValue;
-      }
-    });
-
-    // Apply inputs if available
+  private applyInputs(): void {
     if (this.config.inputs) {
       Object.keys(this.config.inputs).forEach(inputName => {
         const inputValue = this.config.inputs![inputName];
 
-        // Check if the input is an Observable
         if (inputValue instanceof Observable) {
           inputValue.pipe(takeWhile(() => this.isAlive)).subscribe(value => {
-            (componentRef.instance as any)[inputName] = value;
+            (this.componentRef.instance as any)[inputName] = value;
           });
         } else {
-          (componentRef.instance as any)[inputName] = inputValue;
-        }
-      });
-    }
-
-    // Handle specific component types
-    if (this.config.componentType === ComponentType.InputCurrency) {
-      (componentRef.instance as any).formatType = 'currency';
-    }
-
-    if (this.config.componentType === ComponentType.InputNumber) {
-      (componentRef.instance as any).formatType = 'numbers';
-    }
-
-    // Attach form control and form group if available
-    if (this.formGroup) {
-      (componentRef.instance as any).formControlName = this.config.formControlName;
-      (componentRef.instance as any).formGroup = this.formGroup;
-    }
-
-    // Attach event handlers if any
-    if (this.config.outputs) {
-      Object.keys(this.config.outputs).forEach(eventName => {
-        const eventHandler = this.config.outputs![eventName];
-        if (typeof eventHandler === 'function' && (componentRef.instance as any)[eventName]) {
-          (componentRef.instance as any)[eventName].pipe(takeWhile(() => this.isAlive)).subscribe(eventHandler);
+          (this.componentRef.instance as any)[inputName] = inputValue;
         }
       });
     }
   }
 
-  public ngOnDestroy(): void {
-    this.isAlive = false;
+  private applyStandardizedConfig(): void {
+    const standardizedConfig = this.standardizeConfig(this.config);
+
+    Object.keys(standardizedConfig).forEach(inputName => {
+      const inputValue = standardizedConfig[inputName];
+
+      if (inputValue instanceof Observable) {
+        inputValue.pipe(takeWhile(() => this.isAlive)).subscribe(value => {
+          (this.componentRef.instance as any)[inputName] = value;
+        });
+      } else {
+        (this.componentRef.instance as any)[inputName] = inputValue;
+      }
+    });
+  }
+
+  private attachEventHandlers(): void {
+    if (this.config.outputs) {
+      Object.keys(this.config.outputs).forEach(eventName => {
+        const eventHandler = this.config.outputs![eventName];
+        if (typeof eventHandler === 'function' && (this.componentRef.instance as any)[eventName]) {
+          (this.componentRef.instance as any)[eventName].pipe(takeWhile(() => this.isAlive)).subscribe(eventHandler);
+        }
+      });
+    }
+  }
+
+  private attachFormControl(): void {
+    if (this.formGroup) {
+      (this.componentRef.instance as any).formControlName = this.config.formControlName;
+      (this.componentRef.instance as any).formGroup = this.formGroup;
+    }
+  }
+
+  private createComponent(): void {
+    const componentType = this.fieldService.getComponentForField(this.config.componentType) as Type<any>;
+    this.componentRef = this.dynamicField.viewContainerRef.createComponent(componentType);
+
+    this.applyStandardizedConfig();
+    this.applyInputs();
+    this.attachFormControl();
+    this.attachEventHandlers();
   }
 
   private standardizeConfig(config: ControlConfig): { [key: string]: any } {
@@ -115,5 +121,13 @@ export class GtformDynamicFieldComponent implements OnInit, OnDestroy {
     });
 
     return standardizedConfig;
+  }
+
+  private updateComponent(): void {
+    if (this.componentRef) {
+      // Update the component instance with the new config
+      this.applyStandardizedConfig();
+      this.applyInputs();
+    }
   }
 }
